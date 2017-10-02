787d49f2bc35e8fdb0122876eb2320da4155d9a1
Richard Lieu <CeciliaWang@Cecilia-Ximeng-Wangs-MacBook-Pro.local>
2017-10-02 12:08:26 -0700

    update authentication

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -15,29 +15,71 @@ if (sessionStorage.getItem("user")) {
     user = $.parseJSON(sessionStorage.getItem("user"));
 }
 
-function login() {
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        console.log(result);
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        user = result.user;
-        sessionStorage.setItem("user", JSON.stringify(user));
-        // ...
-    }).catch(function(error) {
-        console.log(error);
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
+function onSignIn(googleUser) {
+    console.log('Google Auth Response', googleUser);
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+      unsubscribe();
+      // Check if we are already signed-in Firebase with the correct user.
+      if (!isUserEqual(googleUser, firebaseUser)) {
+        // Build Firebase credential with the Google ID token.
+        var credential = firebase.auth.GoogleAuthProvider.credential(
+            googleUser.getAuthResponse().id_token);
+        // Sign in with credential from the Google user.
+        firebase.auth().signInWithCredential(credential).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+      } else {
+        console.log('User already signed-in Firebase.');
+      }
     });
 }
+
+function isUserEqual(googleUser, firebaseUser) {
+    if (firebaseUser) {
+      var providerData = firebaseUser.providerData;
+      for (var i = 0; i < providerData.length; i++) {
+        if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+            providerData[i].uid === googleUser.getBasicProfile().getId()) {
+          // We don't need to reauth the Firebase connection.
+          return true;
+        }
+      }
+    }
+    return false;
+}
+
+// function login() {
+//     firebase.auth().signInWithPopup(provider).then(function(result) {
+//         console.log(result);
+//         // This gives you a Google Access Token. You can use it to access the Google API.
+//         var token = result.credential.accessToken;
+//         // The signed-in user info.
+//         user = result.user;
+//         sessionStorage.setItem("user", JSON.stringify(user));
+//         // ...
+//     }).catch(function(error) {
+//         console.log(error);
+//         // Handle Errors here.
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//         // The email of the user's account used.
+//         var email = error.email;
+//         // The firebase.auth.AuthCredential type that was used.
+//         var credential = error.credential;
+//         // ...
+//     });
+// }
 $("#test").on("click", function(){
-    login();
+    onSignIn();
+    //login();
 });
 
 // Display form to add listing
