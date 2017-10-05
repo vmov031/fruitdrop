ad3bf8a3f12488377c941ce6a7940ee80a30b18b
Grace Park <gracepark@github.com>
2017-10-04 19:47:48 -0700

    my changes

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
             var email = error.email;
             // The firebase.auth.AuthCredential type that was used.
             var credential = error.credential;
-        });    
+        });
         // Redirect to profile page after login
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
@@ -95,7 +95,6 @@ $(document).ready(function() {
         })
         $('#addListing').modal('hide');
     });
-
     // Display user's listings in profile
     if (currentUser) {
         firebase.database().ref("listings").child(currentUser.uid).on("child_added", function(childSnapshot) {
@@ -106,79 +105,82 @@ $(document).ready(function() {
         });
     }
 
-        // Map Page
-        // Credit: https://wrightshq.com/playground/placing-multiple-markers-on-a-google-map-using-api-3/
+    // Map Page
+    // Credit: https://wrightshq.com/playground/placing-multiple-markers-on-a-google-map-using-api-3/
 
     function initMap() {
-    var map;
-    var infoWindow;
-    var myLatlng1 = new google.maps.LatLng(34.0522,-118.2437);
-    var bounds = new google.maps.LatLngBounds();
-    var mapOptions = {
-        zoom: 10,
-        center: myLatlng1,
-        mapTypeId: 'roadmap'
-    };
-                    
-    // Display a map on the page
-    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    map.setTilt(45);
+        var map;
+        var infoWindow;
+        var myLatlng1 = new google.maps.LatLng(34.0522, -118.2437);
+        var bounds = new google.maps.LatLngBounds();
+        var mapOptions = {
+            zoom: 10,
+            center: myLatlng1,
+            mapTypeId: 'roadmap'
+        };
 
-    // Geolocation
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            map.setCenter(initialLocation);
+        // Display a map on the page
+        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        map.setTilt(45);
+
+        // Geolocation
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                map.setCenter(initialLocation);
+            });
+        }
+
+        // Multiple Markers
+        var markers = [
+            ['Los Angeles, CA', 34.0522, -118.2437],
+            ['Burbank, CA', 34.1808, -118.3090]
+        ];
+
+        // Info Window Content
+        var infoWindowContent = [
+            ['<div class="info_content">' +
+                '<h3>Los Angeles, CA</h3>' +
+                '<p>Get your avocados here!</p>' + '</div>'
+            ],
+            ['<div class="info_content">' +
+                '<h3>Burbank, CA</h3>' +
+                '<p>I have oranges!</p>' +
+                '</div>'
+            ]
+        ];
+
+        // Display multiple markers on a map
+        var infoWindow = new google.maps.InfoWindow(),
+            marker, i;
+
+        // Loop through our array of markers & place each one on the map  
+        for (i = 0; i < markers.length; i++) {
+            var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+            bounds.extend(position);
+            marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                title: markers[i][0]
+            });
+
+            // Allow each marker to have an info window    
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infoWindow.setContent(infoWindowContent[i][0]);
+                    infoWindow.open(map, marker);
+                }
+            })(marker, i));
+
+            // Automatically center the map fitting all markers on the screen
+            map.fitBounds(bounds);
+        }
+
+        // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+        var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+            this.setZoom(14);
+            google.maps.event.removeListener(boundsListener);
         });
-    }
-        
-    // Multiple Markers
-    var markers = [
-        ['Los Angeles, CA', 34.0522,-118.2437],
-        ['Burbank, CA', 34.1808,-118.3090]
-    ];
-                        
-    // Info Window Content
-    var infoWindowContent = [
-        ['<div class="info_content">' +
-        '<h3>Los Angeles, CA</h3>' +
-        '<p>Get your avocados here!</p>' + '</div>'],
-        ['<div class="info_content">' +
-        '<h3>Burbank, CA</h3>' +
-        '<p>I have oranges!</p>' +
-        '</div>']
-    ];
-        
-    // Display multiple markers on a map
-    var infoWindow = new google.maps.InfoWindow(), marker, i;
-    
-    // Loop through our array of markers & place each one on the map  
-    for( i = 0; i < markers.length; i++ ) {
-        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-        bounds.extend(position);
-        marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: markers[i][0]
-        });
-        
-        // Allow each marker to have an info window    
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infoWindow.setContent(infoWindowContent[i][0]);
-                infoWindow.open(map, marker);
-            }
-        })(marker, i));
 
-        // Automatically center the map fitting all markers on the screen
-        map.fitBounds(bounds);
-    }
-
-    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-        this.setZoom(14);
-        google.maps.event.removeListener(boundsListener);
-    });
-        
     }
 });
